80779e6965019ae58ae1d7d61c2eaaea837ebe65
gterzian <2792687+gterzian@users.noreply.github.com>
2023-09-30 16:20:54 +0800

    initial implementation

--- election/src/main.rs
+++ election/src/main.rs
@@ -7,194 +7,93 @@ use axum::{Json, Router};
 use clap::Parser;
 use futures::future::BoxFuture;
 use futures::FutureExt;
-use rand::prelude::*;
 use std::collections::{HashMap, HashSet};
 use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::runtime::Handle;
 use tokio::time::{sleep, Duration};
-use tempfile::TempDir;
+ use std::iter::zip;
 
 async fn get_doc_id(State(state): State<Arc<AppState>>) -> Json<DocumentId> {
     Json(state.doc_handle.document_id())
 }
 
-async fn run_proposal_algorithm(doc_handle: &DocHandle, participant_id: &String) {
-    doc_handle.with_doc_mut(|doc| {
-        let mut synod: Synod = hydrate(doc).unwrap();
-        let our_info = synod.participants.get_mut(participant_id).unwrap();
+fn leader_algorithm(election: &mut Election, participant_id: &String) {
+    let (mut our_epoch, mut our_leadership) = {
+        let our_info = election.participants.get_mut(participant_id).unwrap();
+        (our_info.epoch.clone(), our_info.is_leader.clone())
+    };
+    
+    our_leadership = true;
 
-        // Step 1: ChooseBallotNumber.
-        our_info.last_tried.increment();
-
-        let mut tx = doc.transaction();
-        reconcile(&mut tx, &synod).unwrap();
-        tx.commit();
-    });
-    loop {
-        // Randomly start a new try.
-        if rand::random() {
-            doc_handle.with_doc_mut(|doc| {
-                let mut synod: Synod = hydrate(doc).unwrap();
-                let our_info = synod.participants.get_mut(participant_id).unwrap();
-                
-                // Stop trying once we've written a value to our ledger.
-                if synod.ledger.contains_key(participant_id) {
-                    return;
-                }
-
-                // Step 1: ChooseBallotNumber.
-                our_info.last_tried.increment();
-
-                let mut tx = doc.transaction();
-                reconcile(&mut tx, &synod).unwrap();
-                tx.commit();
-            });
+    for (id, info) in election.participants.iter() {
+        if id == participant_id {
+            continue;
         }
         
-        doc_handle.changed().await.unwrap();
-
-        doc_handle.with_doc_mut(|doc| {
-            let mut synod: Synod = hydrate(doc).unwrap();
-            let participants = synod.participants.clone();
-            let our_info = synod.participants.get_mut(participant_id).unwrap();
-
-            if let Some(ref mut ballot) = our_info.ballot {
-                if ballot.number < our_info.last_tried {
-                    // Forget about older ballots.
-                    our_info.ballot = None;
-                } else {
-                    // Step 5: HandleVoted.
-                    for (id, info) in participants.iter() {
-                        if !ballot.quorum.contains_key(id) {
-                            continue;
-                        }
-                        if let Some(ref vote) = info.prev_vote {
-                            if vote.number == ballot.number && vote.value == ballot.value {
-                                ballot.quorum.insert(id.clone(), true);
-                            }
-                        }
-                    }
-                    let vote_count = ballot.quorum.iter().filter(|(_, voted)| **voted).count();
-                    if vote_count > participants.len() / 2 {
-                        synod
-                            .ledger
-                            .insert(participant_id.clone(), ballot.value.clone());
-                    }
-                }        
-            } else {
-                // Step 3: HandleLastVote.
-                let mut replied: HashMap<String, Option<Vote>> = Default::default();
-                for (id, info) in participants.iter() {
-                    if info.next_bal == our_info.last_tried {
-                        replied.insert(id.clone(), info.prev_vote.clone());
-                    }
-                }
-                if replied.len() > participants.len() / 2 {
-                    // Step 3 (cont'd): BeginBallot.
-                    let mut highest_vote = Vote {
-                        number: Number(0, participant_id.clone()),
-                        value: Value(0),
-                    };
-                    for vote in replied.iter().filter_map(|(_, v)| v.clone()) {
-                        if vote.number > highest_vote.number {
-                            highest_vote = vote.clone();
-                        }
-                    }
-                    let value = if let Value(0) = highest_vote.value {
-                        let mut rng = rand::thread_rng();
-                        Value(rng.gen())
-                    } else {
-                        highest_vote.value
-                    };
-                    let ballot = Ballot {
-                        number: our_info.last_tried.clone(),
-                        value,
-                        quorum: replied.into_iter().map(|(id, _)| (id, false)).collect(),
-                    };
-                    our_info.ballot = Some(ballot);
-                }
+        if participant_id > id {
+            if info.epoch > our_epoch && info.is_leader {
+                our_leadership = false;
             }
-
-            let mut tx = doc.transaction();
-            reconcile(&mut tx, &synod).unwrap();
-            tx.commit();
-        });
+        } else {
+            if info.epoch >= our_epoch {
+                our_leadership = false;
+            }    
+        }
     }
 }
 
-async fn run_acceptor_algorithm(doc_handle: DocHandle, participant_id: &String) {
+async fn run_leader_algorithm(doc_handle: DocHandle, participant_id: String) {
     loop {
         doc_handle.with_doc_mut(|doc| {
-            let mut synod: Synod = hydrate(doc).unwrap();
-            let mut next_bal = synod
-                .participants
-                .get(participant_id)
-                .unwrap()
-                .next_bal
-                .clone();
-
-            for (_id, info) in synod.participants.iter() {
-                if info.last_tried > next_bal {
-                    // Step 2: HandleNextBallot.
-                    next_bal = info.last_tried.clone();
-                }
-            }
-
-            let mut prev_vote = synod
-                .participants
-                .get_mut(participant_id)
-                .unwrap()
-                .prev_vote
-                .clone();
-            for (_id, info) in synod.participants.iter() {
-                if let Some(ref ballot) = info.ballot {
-                    // Step 4: HandleBeginBallot.
-                    if ballot.number == next_bal {
-                        if let Some(ref vote) = prev_vote {
-                            if ballot.number > vote.number {
-                                prev_vote = Some(ballot.clone().into());
-                            }
-                        } else {
-                            prev_vote = Some(ballot.clone().into());
-                        }
-                    }
-                }
-            }
-
-            let our_info = synod.participants.get_mut(participant_id).unwrap();
-            our_info.next_bal = next_bal;
-            our_info.prev_vote = prev_vote;
+            let mut election: Election = hydrate(doc).unwrap();
+            
+            leader_algorithm(&mut election, &participant_id);
+            
+            let confirmed_leaders = election.participants.values().filter(|info| info.is_leader).count();
+            assert_eq!(confirmed_leaders, 1);
+            
+            // Update epoch to max.
+            let max_epoch = election.participants.values().map(|info| info.epoch).max().unwrap();
+            let our_info = election.participants.get_mut(&participant_id).unwrap();
+            our_info.epoch = max_epoch;
 
             let mut tx = doc.transaction();
-            reconcile(&mut tx, &synod).unwrap();
+            reconcile(&mut tx, &election).unwrap();
             tx.commit();
         });
         doc_handle.changed().await.unwrap();
     }
 }
 
-async fn run_learner_algorithm(doc_handle: DocHandle, participant_id: String) {
+async fn run_heartbeat_algorithm(doc_handle: DocHandle, participant_id: String) {
     loop {
+        sleep(Duration::from_millis(1000)).await;
         doc_handle.with_doc_mut(|doc| {
-            let mut synod: Synod = hydrate(doc).unwrap();
-            let mut values: HashSet<Value> = synod.ledger.values().cloned().collect();
-            if !values.is_empty() {
-                assert_eq!(values.len(), 1);
-
-                // Step 6: HandleSuccess.
-                synod
-                    .ledger
-                    .insert(participant_id.clone(), values.drain().next().unwrap());
-            }
-
-            println!("Ledger: {:?}", synod.ledger);
+            let mut election: Election = hydrate(doc).unwrap();
+            
+            let our_info = election.participants.get_mut(&participant_id).unwrap();
+            our_info.epoch += 1;
 
             let mut tx = doc.transaction();
-            reconcile(&mut tx, &synod).unwrap();
+            reconcile(&mut tx, &election).unwrap();
             tx.commit();
         });
-        doc_handle.changed().await.unwrap();
+    }
+}
+
+async fn run_crash_algorithm(doc_handle: DocHandle, participant_id: String) {
+    loop {
+        sleep(Duration::from_millis(10000)).await;
+        doc_handle.with_doc_mut(|doc| {
+            let mut election: Election = hydrate(doc).unwrap();
+            
+            let our_info = election.participants.entry(participant_id.clone()).and_modify(|info| *info = Default::default());
+
+            let mut tx = doc.transaction();
+            reconcile(&mut tx, &election).unwrap();
+            tx.commit();
+        });
     }
 }
 
@@ -211,103 +110,42 @@ struct AppState {
     doc_handle: DocHandle,
 }
 
-#[derive(Debug, Clone, Reconcile, Hydrate, Eq, Hash, PartialEq, Ord, PartialOrd)]
-struct Number(u32, String);
-
-impl Number {
-    fn increment(&mut self) {
-        self.0 += 1;
-    }
-}
-
-#[derive(Debug, Clone, Reconcile, Hydrate, Eq, Hash, PartialEq)]
-struct Value(u32);
-
-#[derive(Debug, Clone, Reconcile, Hydrate, PartialEq)]
-struct Vote {
-    number: Number,
-    value: Value,
-}
-
-impl From<Ballot> for Vote {
-    fn from(ballot: Ballot) -> Self {
-        Vote {
-            number: ballot.number,
-            value: ballot.value,
-        }
-    }
-}
-
-#[derive(Debug, Clone, Reconcile, Hydrate)]
-struct Ballot {
-    number: Number,
-    value: Value,
-    quorum: HashMap<String, bool>,
-}
-
-#[derive(Debug, Clone, Reconcile, Hydrate)]
+#[derive(Debug, Clone, Reconcile, Hydrate, Default)]
 struct Participant {
-    last_tried: Number,
-    next_bal: Number,
-    prev_vote: Option<Vote>,
-    ballot: Option<Ballot>,
+    pub epoch: u64,
+    pub is_leader: bool,
 }
 
 #[derive(Default, Debug, Clone, Reconcile, Hydrate)]
-struct Synod {
+struct Election {
     pub participants: HashMap<String, Participant>,
-    pub ledger: HashMap<String, Value>,
 }
 
-struct BlockingFsStorage(Arc<Mutex<FsStore>>);
+struct NoStorage;
 
-impl Storage for BlockingFsStorage {
-    fn get(&self, id: DocumentId) -> BoxFuture<'static, Result<Option<Vec<u8>>, StorageError>> {
-        Box::pin(futures::future::ready(
-            self.0
-                .lock()
-                .unwrap()
-                .get(&id)
-                .map_err(move |_| StorageError::Error),
-        ))
+impl Storage for NoStorage {
+    fn get(&self, _id: DocumentId) -> BoxFuture<'static, Result<Option<Vec<u8>>, StorageError>> {
+        Box::pin(futures::future::ready(Ok(None)))
     }
 
     fn list_all(&self) -> BoxFuture<'static, Result<Vec<DocumentId>, StorageError>> {
-        Box::pin(futures::future::ready(
-            self.0
-                .lock()
-                .unwrap()
-                .list()
-                .map_err(move |_| StorageError::Error),
-        ))
+        Box::pin(futures::future::ready(Ok(vec![])))
     }
 
     fn append(
         &self,
-        id: DocumentId,
-        changes: Vec<u8>,
+        _id: DocumentId,
+        _changes: Vec<u8>,
     ) -> BoxFuture<'static, Result<(), StorageError>> {
-        Box::pin(futures::future::ready(
-            self.0
-                .lock()
-                .unwrap()
-                .append(&id, &changes)
-                .map_err(move |_| StorageError::Error),
-        ))
+        Box::pin(futures::future::ready(Ok(())))
     }
 
     fn compact(
         &self,
-        id: DocumentId,
-        full_doc: Vec<u8>,
+        _id: DocumentId,
+        _full_doc: Vec<u8>,
     ) -> BoxFuture<'static, Result<(), StorageError>> {
-        Box::pin(futures::future::ready(
-            self.0
-                .lock()
-                .unwrap()
-                .compact(&id, &full_doc)
-                .map_err(move |_| StorageError::Error),
-        ))
+        Box::pin(futures::future::ready(Ok(())))
     }
 }
 
@@ -341,14 +179,7 @@ async fn main() {
     let our_tcp_addr = format!("127.0.0.1:234{}", participant_id);
 
     // Create a repo.
-    let temp_dir = TempDir::new().unwrap();
-    let store = FsStore::open(temp_dir.path()).unwrap();
-    let repo = Repo::new(
-        None,
-        Box::new(BlockingFsStorage(Arc::new(Mutex::new(
-            store
-        )))),
-    );
+    let repo = Repo::new(None, Box::new(NoStorage));
     let repo_handle = repo.run();
 
     // Start a tcp server.
@@ -388,24 +219,18 @@ async fn main() {
     });
 
     let doc_handle = if bootstrap {
-        let mut synod: Synod = Default::default();
+        let mut election: Election = Default::default();
         for participant_id in customers.clone() {
-            let participant = Participant {
-                last_tried: Number(0, participant_id.clone()),
-                next_bal: Number(0, participant_id.clone()),
-                prev_vote: None,
-                ballot: None,
-            };
-            synod
+            election
                 .participants
-                .insert(participant_id.to_string(), participant);
+                .insert(participant_id.to_string(), Default::default());
         }
 
         // The initial document.
         let doc_handle = repo_handle.new_document();
         doc_handle.with_doc_mut(|doc| {
             let mut tx = doc.transaction();
-            reconcile(&mut tx, &synod).unwrap();
+            reconcile(&mut tx, &election).unwrap();
             tx.commit();
         });
 
@@ -439,17 +264,19 @@ async fn main() {
     let doc_handle_clone = doc_handle.clone();
     let id = participant_id.clone();
     handle.spawn(async move {
-        run_proposal_algorithm(&doc_handle_clone, &id).await;
+        run_heartbeat_algorithm(doc_handle_clone, id).await;
     });
-
+    
     let doc_handle_clone = doc_handle.clone();
     let id = participant_id.clone();
     handle.spawn(async move {
-        run_acceptor_algorithm(doc_handle_clone, &id).await;
+        run_crash_algorithm(doc_handle_clone, id).await;
     });
-
+    
+    let doc_handle_clone = doc_handle.clone();
+    let id = participant_id.clone();
     handle.spawn(async move {
-        run_learner_algorithm(doc_handle, participant_id).await;
+        run_leader_algorithm(doc_handle_clone, id).await;
     });
 
     let app = Router::new()
